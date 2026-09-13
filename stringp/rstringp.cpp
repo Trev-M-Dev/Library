19eb4360edbfea668decec1e82c9075eae9ae3a9
Dev_Trevor <trevormbagari2@gmail.com>
2026-09-13 14:57:50 +0300

    Adds missing return reverse method (NEEDS POLISHING)

--- stringp/rstringp.cpp
+++ stringp/rstringp.cpp
@@ -26,3 +26,18 @@ std::string stringp::r_upper() {
     }
     return temp;
 }
+
+std::string stringp::r_reverse() {
+    std::string reversed = "";
+    char reversal[this->length()];
+    int ln1 = (this->length()) - 1;
+    for(char& c : *this) {
+        reversal[ln1] = c;
+        ln1--;
+    }
+
+    for(int i = 0; i < this->length(); i++) {
+        reversed = reversed + reversal[i];
+    }
+    return reversed;
+}
