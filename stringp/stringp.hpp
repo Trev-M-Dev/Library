a51e348f52408aeb675f0b0db9ff076badbf5ff9
Dev_Trevor <trevormbagari2@gmail.com>
2026-09-15 21:59:58 +0300

    Minor codebase cleanup

--- stringp/stringp.hpp
+++ stringp/stringp.hpp
@@ -13,15 +13,17 @@ public:
     //Static Variables
     static constexpr char alpha_Upper[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     static constexpr char alpha_Lower[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    //Custom Methods
+    /*Custom Methods*/
+    //Mutators
     void lower();
-    std::string r_lower();
     void upper();
-    std::string r_upper();
     void reverse();
+    void replace(std::string srcString, std::string subString);
+    //Accessors
+    std::string r_lower();
+    std::string r_upper();
     std::string r_reverse();
     std::vector<std::string> split();
-    void replace(std::string srcString, std::string subString);
 
 };
 #endif
